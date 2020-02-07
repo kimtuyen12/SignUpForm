e6198f75414733e2e54f3c6df58db815b7ce5713
Tuyen Pham <tuh39709@temple.edu>
2020-02-07 17:12:38 -0500

    name to FormActivity

--- app/src/main/java/temple/edu/signupform/FormActivity.java
+++ app/src/main/java/temple/edu/signupform/FormActivity.java
@@ -3,6 +3,7 @@ package temple.edu.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final View name = findViewById(R.id.editTextName);
+        final View email = findViewById(R.id.editTextEmail);
+        final View password = findViewById(R.id.editTextPassword);
+        final View passwordConf = findViewById(R.id.editTextPasswordConf);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
